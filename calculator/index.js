9f188234276db3d373957b1b8e6bc799c374e3b5
kerkazou <kerkazou.zakaria@gmail.com>
2022-03-11 00:06:22 +0100

    suit de brief calculatrice

--- calculator/index.js
+++ calculator/index.js
@@ -29,8 +29,10 @@ function dark(){
 
 // historique
 function sup_historique() {
-   hoperation.value = "";
-   hresult.value = "";
+   var hoperation = document.getElementById("hoperation");
+   var hresult = document.getElementById("hresult");
+   hoperation.innerText = "";
+   hresult.innerText = "";
 }
 
 // calculatrice
@@ -38,18 +40,37 @@ function btmCl(){
    result.value = "";
 }
 
+function del() {
+   result.value = result.value.slice(0, -1);
+}
+
 function btm(val) {
    result.value += val;
-   // document.getElementById("hoperation").innerHTML = "<h3>" + result.value + "<h3>";
 }
 
 function operation(operation) {
    result.value += operation;
-   // document.getElementById("hoperation").innerHTML = "<h3>" + result.value + "<h3>";
+   var search_addition = result.value.includes("+");
+   var search_soustraction = result.value.includes("-");
+   var search_multiplication = result.value.includes("*");
+   var search_division = result.value.includes("/");
+   var search_pourcentage = result.value.includes("%");
+   if(search_addition || search_soustraction || search_multiplication || search_division || search_pourcentage){
+      document.getElementById("addition_operation").disabled =
+      document.getElementById("soustraction_operation").disabled =
+      document.getElementById("multiplication_operation").disabled =
+      document.getElementById("division_operation").disabled =
+      document.getElementById("pourcentage_operation").disabled = true;
+   }
 }
 
 function btmFloat() {
    result.value += ".";
+   var search_float = result.value.includes(".");
+   if(search_float){
+      console.log("flaut is here");
+      // document.getElementById("btm_float").disabled = true;
+   }
 }
 
 function btmEgal() {
@@ -57,19 +78,82 @@ function btmEgal() {
    var egal = eval(result.value);
    result.value = egal;
    document.getElementById("hresult").innerHTML += "<h3>" + "=" +  result.value + "<h3>";
+   document.getElementById("addition_operation").disabled =
+   document.getElementById("soustraction_operation").disabled =
+   document.getElementById("multiplication_operation").disabled =
+   document.getElementById("division_operation").disabled =
+   document.getElementById("pourcentage_operation").disabled = false;
 }
 
+   const addition = /\+/;
+   const soustraction = /\-/;
+   const division = /\//;
+   const multiplication = /\*/;
+   const pourcentage = /\%/;
 
-// test
-// var res = "1+2";
-// var op = res.search(/1/);
-// window.test.innerHTML = op;
+      // switch (operation) {
+   //    case '+':
+   //       var type_operation = "+";
+   //    break
+   //    case '-':
+   //       var type_operation = "-";
+   //    break
+   //    case '*':
+   //       var type_operation = "*";
+   //    break
+   //    case '/':
+   //       var type_operation = "/";
+   //    break
+   //    case '%':
+   //       var type_operation = "%";
+   //    break
+   //    default:
+   // }
+   // console.log(type_operation);
 
-// var result = window.result.value;
-// var patt = /dark/;
-// var x = patt.result(result);
-// if(x){
-//    window.h.innerHTML = "true"
-// }else{
-//    window.h.innerHTML = "false"
-// }
+   
+
+   //  confirmation operation
+   // var test = "+13312";
+   // const conf_operation = /(\+|\-|\/|\%|\*)[0-9]/g;
+   // if(conf_operation.test(test)){
+   //    console.log("your operation is correct");
+   // }else{
+   //    console.log("your operation is incorrect");
+   // }
+
+   // pour egal
+   // const conf_operation = /[0-9](\+|\-|\/|\%|\*|\.)[0-9]/g;
+
+   // pour operation
+   // const conf_operation = /[0-9]/g;
+
+   // pour la firgule
+   // const conf_operation = /(\+|\-|\/|\%|\*)[0-9]/g;
+
+
+   // var test = "12 3";
+   // const regex = " ";
+   // const n1_n2 = test.split(regex);
+   // console.log(n1_n2);
+   // var n1 = n1_n2[0];
+   // var n2 = n1_n2[1];
+   // switch ("+") {
+   //    case '+':
+   //       result.value = n1_n2[0] + n1_n2[1] ;
+   //    break
+   //    case '-':
+   //       result.value = n1 - n2;
+   //    break
+   //    case '*':
+   //       result.value = n1 * n2;
+   //    break
+   //    case '/':
+   //       result.value = n1 / n2;
+   //    break
+   //    case '%':
+   //       result.value = n1 % n2;
+   //    break
+   //    default:
+   // }
+   // console.log(result.value);
